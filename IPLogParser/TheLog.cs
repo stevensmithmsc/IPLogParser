5c1cc5ce45dacbf138584ddb342052cd654e4d8e
Steven Smith <steven.smith5@PEN-20938-LAP>
2016-05-09 13:09:17 +0100

    Project Setup

---
+++ IPLogParser/TheLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPLogParser
+{
+    class TheLog
+    {
+        ObservableCollection<LogEntry> LogList { get; }
+
+        public bool readfromfile(string filename)
+        {
+            return false;
+        }
+    }
+}
