ae4fec3bb0e11ada89e62971124ce6a4b6f60006
Steven Smith <steven.smith5@PEN-20938-LAP>
2016-05-10 16:51:23 +0100

    Parses file into individual entries
    will not parse file into individual entries, need to enable notification
    on non-list fields.

--- IPLogParser/LogEntry.cs
+++ IPLogParser/LogEntry.cs
@@ -8,6 +8,40 @@ namespace IPLogParser
 {
     public class LogEntry
     {
-        string Entry { get; set; }
+        public string Entry { get; set; }
+        public bool hasError { get; private set; }  //determines if entry contains an error message
+        private bool doneCount;                     //determines if Counted.... line has been reached
+
+        public LogEntry(string startString)
+        {
+            Entry = startString;
+            doneCount = false;
+            hasError = false;
+        }
+
+        public LogEntry()
+        {
+            Entry = "";
+            doneCount = false;
+            hasError = false;
+        }
+
+        public void AddLine(string newString)
+        {
+            bool addNewLine = false;
+            if ((!string.IsNullOrEmpty(newString) && char.IsUpper(newString[0]))||(newString.Length>2 && newString[1] == '-')) addNewLine = true;
+            if (doneCount) addNewLine = true;
+            if (Entry == "") addNewLine = false;
+
+            Entry += addNewLine ? Environment.NewLine + newString : newString;
+
+            if (newString.Length > 7 && newString.Substring(0, 7) == "Counted") doneCount = true;
+            if (doneCount && newString.Contains("error")) hasError = true;
+        }
+
+        override public string  ToString()
+        {
+            return Entry;
+        }
     }
 }
--- IPLogParser/TheLog.cs
+++ IPLogParser/TheLog.cs
@@ -12,35 +12,80 @@ namespace IPLogParser
     {
         public ObservableCollection<LogEntry> LogList { get; private set; }
         public string stupidString { get; private set; }
+        public int noEntries { get { return LogList.Count; } }
+        public int noErrors { get; private set;}
 
         public TheLog()
         {
             stupidString = "Hello";
-            this.readFromFile("C:\\Users\\Steve\\Source\\Repos\\IPLogParser\\IPLogParser\\Data\\inpatient_testrun_20160504_5.log");
+            LogList = new ObservableCollection<LogEntry>();
+            LogList.Add(new LogEntry("Stupid"));
+            LogList.Add(new LogEntry("Rubbish"));
+            noErrors = 0;
+            this.readFromFile("C:\\Users\\steven.smith\\Source\\Repos\\IPLogParser\\IPLogParser\\Data\\inpatient_testrun_20160504.log");
         }
 
         public async void readFromFile(string filename)
         {
-            stupidString = "";
+            LogList.Clear();
+            noErrors = 0;
             try
-            {   // Open the text file using a stream reader.
+            {   
+                LogEntry currentEntry = new LogEntry();                
+                // Open the text file using a stream reader.
                 using (StreamReader sr = new StreamReader(filename))
                 {
+
                     while(!sr.EndOfStream)
                     {
                         String line = await sr.ReadLineAsync();
-                        stupidString += line;
-                        stupidString += Environment.NewLine;
-                    }
-                    
+                        if (line.Length > 8 && line.Substring(0,8)=="Spell ID")
+                        {
+                            if (currentEntry.hasError) noErrors++;
+                            LogList.Add(currentEntry);
+                            currentEntry = new LogEntry(line);
+                        } else
+                        {
+                            currentEntry.AddLine(line);
+                        }
+                    }     
                 }
+                stupidString = filename;
+                if (currentEntry.hasError) noErrors++;
+                LogList.Add(currentEntry);
+                
             }
             catch (Exception e)
             {
-                stupidString ="The file could not be read:";
+                stupidString = "The file could not be read:\n";
                 stupidString += e.Message;
             }
-            stupidString += "\n";
+            
         }
     }
 }
+
+
+
+/*
+ * http://www.c-sharpcorner.com/uploadfile/mahesh/openfiledialog-in-wpf/
+// Create OpenFileDialog 
+
+Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();            
+
+// Set filter for file extension and default file extension 
+dlg.DefaultExt = ".txt"; 
+dlg.Filter = "Text documents (.txt)|*.txt";  
+
+
+// Display OpenFileDialog by calling ShowDialog method 
+Nullable<bool> result = dlg.ShowDialog(); 
+
+// Get the selected file name and display in a TextBox 
+if (result == true) 
+{ 
+    // Open document 
+    string filename = dlg.FileName; 
+    FileNameTextBox.Text = filename; 
+ }
+*/
